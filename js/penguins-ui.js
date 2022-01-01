e3b7097a9dd6711056f82faadab5b4f80f9efa49
Art Ste <art.ste.web@gmail.com>
2022-01-01 22:40:11 +0200

    add tabs docs

--- js/penguins-ui.js
+++ js/penguins-ui.js
@@ -15,14 +15,23 @@ function tabsHandler() {
         tabsTitles[i].addEventListener('click', showTabContent);
         
     }
+    let elHeights = [];
     for(let i=0; i<tabsContents.length; i++) {
-         tabsContents[i].setAttribute('data-content-num', [i]);
+        tabsContents[i].setAttribute('data-content-num', [i]);
+        console.log(tabsContents[i].offsetHeight);
+        elHeights.push(tabsContents[i].offsetHeight);
     }
-
-
-    console.log(tabsTitles);
+    
+    console.log(elHeights);
+    let maxHeight = getMaxHeight(elHeights);
+    tabs.style.height = maxHeight+120+'px';
+    console.log(maxHeight);
     console.log(tabsContents);
 }
+function getMaxHeight(elHeights) {
+    return Math.max.apply(null, elHeights);
+}
+
 
 function showTabContent(e) {
     const tabsTitles = document.querySelectorAll('.tab-title');
@@ -37,9 +46,10 @@ function showTabContent(e) {
     console.log(currentTabNum);
     for(let i=0; i<tabsContents.length; i++) {
         tabsContents[i].classList.remove('show-content');
+        tabsContents[i].style.display = 'none';
         if(i==currentTabNum) {
             tabsContents[i].classList.add('show-content');
-
+            tabsContents[i].style.display = 'block';
         }
     }
 }
